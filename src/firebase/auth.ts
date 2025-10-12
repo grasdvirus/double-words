0f89d5370bfeb06b82a7efeed0a6d3d416266b03
Venom Grasd <grasdvenom@gmail.com>
2025-10-12 12:20:48 +0000

    je crois que la méthode de connexion n'est pas bien synchronisé avec le

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -11,7 +11,11 @@ const provider = new GoogleAuthProvider();
 export function signInWithGoogle(auth: Auth) {
   // Utilise la redirection, qui est plus fiable en production et sur mobile
   signInWithRedirect(auth, provider).catch(error => {
-    console.error("Erreur de redirection de connexion Google : ", error);
+    // Cette erreur se produit si l'utilisateur ferme la pop-up.
+    // Nous pouvons l'ignorer en toute sécurité.
+    if (error.code !== 'auth/popup-closed-by-user') {
+        console.error("Erreur de redirection de connexion Google : ", error);
+    }
   });
 }
 
