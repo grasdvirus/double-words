743ba1eb147a4f67e92aba253695a625bf96b70c
Venom Grasd <grasdvenom@gmail.com>
2025-10-13 21:41:12 +0000

    ajoute ce nouveau mode de chargement au site pour une élégance visuel. .

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,30 @@
-import { LoaderCircle } from 'lucide-react';
-
 export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <LoaderCircle className="h-12 w-12 animate-spin text-primary" />
+      <div className="section-center">
+        <div className="section-path">
+          <div className="globe">
+            <div className="wrapper">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
