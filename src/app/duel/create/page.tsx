a03c6159e904b1a0c07ffcd137c3ed423f822452
Venom Grasd <grasdvenom@gmail.com>
2025-10-14 14:12:04 +0000

    ReferenceError: useEffect is not defined     at CreateDuelPage (https://

--- src/app/duel/create/page.tsx
+++ src/app/duel/create/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useUser, useFirestore } from '@/firebase';
 import { collection, addDoc, serverTimestamp, doc, onSnapshot } from 'firebase/firestore';
