f0014b1b21d275bceb8d53db74e47fd24d56a713
Venom Grasd <grasdvenom@gmail.com>
2025-10-15 16:59:20 +0000

    supprime la permissions des saisir de texte et zoom sur le site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Double Words",
   description: "Le jeu où les lettres se répètent, mais jamais les idées.",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({
